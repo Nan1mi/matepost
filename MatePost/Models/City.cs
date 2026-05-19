e01e748dd3efecc3d92005aab2f6ece93a9ce43b
Milana <malytsiamilana1@gmail.com>
2026-05-20 01:18:53 +0200

    Update City.cs

--- MatePost/Models/City.cs
+++ MatePost/Models/City.cs
@@ -1,7 +1,11 @@
-﻿namespace MatePost.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace MatePost.Models;
 public class City
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Region { get; set; } = string.Empty;
